9cde33949846101083290c8ff12a06d48f4150de
Mike Landers <landers.mike@gmail.com>
2019-05-01 15:47:44 -0700

    stock prices solved

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,34 +3,24 @@
 import argparse
 
 
-# def find_max_profit(prices):
-#     result = 0
-#     index = 0
-#     for x in prices:
-#         for y in range(index, len(prices)):
-#             print(x, prices[y])
-#             if x < prices[y]:
-#                 if prices[y]-x > result:
-#                     result = prices[y]-x
-#         index += 1
-#     return result
-
 def find_max_profit(prices):
-    cur_min = prices[0]
-    result = 0
+    result = []
+    index = 0
     for x in prices:
-        print(x)
-        if x < cur_min:
-            cur_min = x
-            print(x, cur_min)
-            print(x-cur_min)
+        for y in range(index, len(prices)):
+            if x == prices[y]:
+                continue
+            else:
+                result.append(prices[y]-x)
+        index += 1
 
-    return result
+    maxProfits = max(result)
+    return maxProfits
 
 
 # print(find_max_profit([10, 7, 5, 8, 11, 9]))
-print(find_max_profit([100, 90, 80, 50, 20, 10]))
-# print(find_max_profit([1050, 270, 1540, 3800, 2]))
+# print(find_max_profit([100, 90, 80, 50, 20, 10]))
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 # print(find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]))
 
 if __name__ == '__main__':
